2af2607b4852c6c3ed872b1076a275717792389c
Lazarus2179 <micael.lazaro@gmail.com>
2023-09-14 06:40:24 +0100

    Display addiction question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,30 @@ let buttons = document.getElementsByTagName("button");
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                 alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
-         })
-     }
-})
+         });
+        
+        }
+  
+  runGame("addiction")      
+});
+/**
+* The main game "loop", called when the script is first loaded
+* and after the user's answer has been processed 
+*/
+function runGame(gameType) {
 
-function runGame() {
+    //Creates two random number between 1 and 25
+let num1 = Math.floor(Math.random() * 25) +1;
+let num2 = Math.floor(Math.random() * 25) +1;
+
+if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+} else {
+    alert(`Unknow game type: ${gameType}`);
+    throw `Unknow game type: ${gameType}. Aborting!`;
+}
 
 }
 
@@ -42,7 +59,10 @@ function incrementWrongScore() {
 }
 
 
-function displayAddictionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "+";
 
 }
 
